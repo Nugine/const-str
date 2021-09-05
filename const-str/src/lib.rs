50326d86c1e9b25af7f094e5133cf87f789fafc8
Nugine <nugine@foxmail.com>
2021-09-05 15:37:40 +0800

    refactor: proc impl to_lowercase!, to_uppercase!, replace!

--- const-str/src/lib.rs
+++ const-str/src/lib.rs
@@ -137,3 +137,60 @@ macro_rules! from_utf8 {
         $crate::__proc::from_utf8!($s)
     };
 }
+
+// -----------------------------------------------------------------------------
+
+/// Returns the lowercase equivalent of this string literal, as a new string literal.
+///
+/// See [`str::to_lowercase`](https://doc.rust-lang.org/std/primitive.str.html#method.to_lowercase).
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!("hello", const_str::to_lowercase!("HELLO"));
+/// ```
+///
+#[macro_export]
+macro_rules! to_lowercase {
+    ($s: literal) => {
+        $crate::__proc::to_lowercase!($s)
+    };
+}
+
+/// Returns the uppercase equivalent of this string literal, as a new string literal.
+///
+/// See [`str::to_uppercase`](https://doc.rust-lang.org/std/primitive.str.html#method.to_uppercase).
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!("HELLO", const_str::to_uppercase!("hello"));
+/// ```
+///
+#[macro_export]
+macro_rules! to_uppercase {
+    ($s: literal) => {
+        $crate::__proc::to_uppercase!($s)
+    };
+}
+
+// -----------------------------------------------------------------------------
+
+/// Replaces all matches of a pattern with another string literal.
+///
+/// See [`str::replace`](https://doc.rust-lang.org/std/primitive.str.html#method.replace).
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!("this is new", const_str::replace!("this is old", "old", "new"));
+/// ```
+///
+#[macro_export]
+macro_rules! replace {
+    ($s: literal, $from: literal, $to: literal) => {
+        $crate::__proc::replace!($s, $from, $to)
+    };
+}
+
+// -----------------------------------------------------------------------------

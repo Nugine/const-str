80037ecd47e2e2d77d1f68881d9f343a5aa2f2fb
Nugine <nugine@foxmail.com>
2021-11-02 22:56:21 +0800

    optimize uuid parser

--- const-str/src/__ctfe/uuid.rs
+++ const-str/src/__ctfe/uuid.rs
@@ -6,23 +6,15 @@ pub struct Error(ErrorKind);
 #[derive(Debug, PartialEq, Eq)]
 enum ErrorKind {
     InvalidLength,
-    InvalidCharacter {
-        expected: &'static str,
-        found: char,
-        index: usize,
-    },
+    InvalidCharacter,
 }
 
 const fn invalid_length() -> Error {
     Error(ErrorKind::InvalidLength)
 }
 
-const fn invalid_character(expected: &'static str, found: char, index: usize) -> Error {
-    Error(ErrorKind::InvalidCharacter {
-        expected,
-        found,
-        index,
-    })
+const fn invalid_character() -> Error {
+    Error(ErrorKind::InvalidCharacter)
 }
 
 const fn strip_prefix<'a>(s: &'a str, prefix: &str) -> Option<&'a [u8]> {
@@ -47,7 +39,8 @@ const fn try_parse_uuid(s: &str) -> Result<[u8; 16], Error> {
         45 => match strip_prefix(s, "urn:uuid:") {
             Some(s) => parse_uuid_hyphenated(s),
             None => Err(invalid_length()),
-        }, // Microsoft GUID
+        },
+        // Microsoft GUID
         38 => match s.as_bytes() {
             [b'{', xs @ .., b'}'] => parse_uuid_hyphenated(xs),
             _ => Err(invalid_length()),
@@ -60,27 +53,52 @@ const fn try_parse_uuid(s: &str) -> Result<[u8; 16], Error> {
     }
 }
 
-const HEX_DIGITS: &str = "0123456789abcdefABCDEF";
+type Table = [u8; 256];
 
-const fn parse_hex_digit(s: &[u8], i: usize) -> Result<u8, Error> {
-    let d = s[i];
-    match d {
-        b'0'..=b'9' => Ok(d - b'0'),
-        b'a'..=b'f' => Ok(d - b'a' + 10),
-        b'A'..=b'F' => Ok(d - b'A' + 10),
-        _ => Err(invalid_character(HEX_DIGITS, d as char, i)),
+const fn generate_lookup_table() -> Table {
+    let mut buf: [u8; 256] = [0; 256];
+    let mut i: u8 = 0;
+    loop {
+        buf[i as usize] = match i {
+            b'0'..=b'9' => i - b'0',
+            b'a'..=b'f' => i - b'a' + 10,
+            b'A'..=b'F' => i - b'A' + 10,
+            _ => 0xff,
+        };
+        if i == 255 {
+            return buf;
+        }
+        i += 1
     }
 }
+
+const HEX_TABLE: Table = generate_lookup_table();
+
+const fn generate_shl4_table() -> Table {
+    let mut buf: [u8; 256] = [0; 256];
+    let mut i: u8 = 0;
+    loop {
+        buf[i as usize] = i.wrapping_shl(4);
+        if i == 255 {
+            return buf;
+        }
+        i += 1;
+    }
+}
+
+const SHL4_TABLE: Table = generate_shl4_table();
 
 const fn parse_uuid_simple(s: &[u8]) -> Result<[u8; 16], Error> {
     constfn_assert!(s.len() == 32);
     let mut buf: [u8; 16] = [0; 16];
     let mut i = 0;
-    while i < 32 {
-        match parse_hex_digit(s, i) {
-            Ok(x) => buf[i / 2] = (buf[i / 2] * 16) + x,
-            Err(err) => return Err(err),
-        };
+    while i < 16 {
+        let h1 = HEX_TABLE[s[i * 2] as usize];
+        let h2 = HEX_TABLE[s[i * 2 + 1] as usize];
+        if h1 | h2 == 0xff {
+            return Err(invalid_character());
+        }
+        buf[i] = SHL4_TABLE[h1 as usize] | h2;
         i += 1;
     }
     Ok(buf)
@@ -89,35 +107,27 @@ const fn parse_uuid_simple(s: &[u8]) -> Result<[u8; 16], Error> {
 const fn parse_uuid_hyphenated(s: &[u8]) -> Result<[u8; 16], Error> {
     constfn_assert!(s.len() == 36);
 
-    const GROUPS: [(usize, usize); 5] = [(0, 8), (9, 13), (14, 18), (19, 23), (24, 36)];
+    // b'-' is 0x2d
+    let x = u32::from_ne_bytes([s[8], s[13], s[18], s[23]]);
+    if x != 0x2d2d2d2d {
+        return Err(invalid_character());
+    }
 
+    let positions: [u8; 8] = [0, 4, 9, 14, 19, 24, 28, 32];
     let mut buf: [u8; 16] = [0; 16];
     let mut j = 0;
-
-    let mut group = 0;
-    while group < 5 {
-        let (start, end) = GROUPS[group];
-
-        let mut i = start;
-        while i < end {
-            match parse_hex_digit(s, i) {
-                Ok(x) => {
-                    buf[j / 2] = (buf[j / 2] * 16) + x;
-                    j += 1;
-                }
-                Err(err) => return Err(err),
-            };
-            i += 1;
+    while j < 8 {
+        let i = positions[j];
+        let h1 = HEX_TABLE[s[i as usize] as usize];
+        let h2 = HEX_TABLE[s[(i + 1) as usize] as usize];
+        let h3 = HEX_TABLE[s[(i + 2) as usize] as usize];
+        let h4 = HEX_TABLE[s[(i + 3) as usize] as usize];
+        if h1 | h2 | h3 | h4 == 0xff {
+            return Err(invalid_character());
         }
-
-        if group < 4 {
-            let d = s[end];
-            if d != b'-' {
-                return Err(invalid_character("-", d as char, end));
-            }
-        }
-
-        group += 1;
+        buf[j * 2] = SHL4_TABLE[h1 as usize] | h2;
+        buf[j * 2 + 1] = SHL4_TABLE[h3 as usize] | h4;
+        j += 1;
     }
 
     Ok(buf)
@@ -132,6 +142,7 @@ fn test_parse_uuid() {
         (A1, "67e55044-10b1-426f-9247-bb680e5fe0c8"),
         (A1, "67e5504410b1426f9247bb680e5fe0c8"),
         (A1, "{67e55044-10b1-426f-9247-bb680e5fe0c8}"),
+        (A1, "urn:uuid:67e55044-10b1-426f-9247-bb680e5fe0c8"),
         (A2, "00000000000000000000000000000000"),
         (A2, "00000000-0000-0000-0000-000000000000"),
         (
@@ -154,6 +165,8 @@ fn test_parse_uuid() {
     }
 
     const B1: Error = invalid_length();
+    const B2: Error = invalid_character();
+
     const ERR: &[(Error, &str)] = &[
         (B1, ""),
         (B1, "!"),
@@ -166,54 +179,18 @@ fn test_parse_uuid() {
         (B1, "67e5504410b1426f9247bb680e5fe0cg8"),
         (B1, "{00000000000000000000000000000000}"),
         (B1, "67e5504410b1426f9247bb680e5fe0c"),
-        (
-            invalid_character("-", '1', 23),
-            "F9168C5E-CEB2-4faa-B6BF1-02BF39FA1E4",
-        ),
-        (
-            invalid_character("-", '2', 8),
-            "231231212212423424324323477343246663",
-        ),
-        (
-            invalid_character(HEX_DIGITS, '-', 8),
-            "01020304-1112-2122-3132-41424344",
-        ),
-        (
-            invalid_character(HEX_DIGITS, 'G', 20),
-            "F9168C5E-CEB2-4faa-BGBF-329BF39FA1E4",
-        ),
-        (
-            invalid_character("-", 'F', 13),
-            "F9168C5E-CEB2F4faaFB6BFF329BF39FA1E4",
-        ),
-        (
-            invalid_character("-", 'F', 18),
-            "F9168C5E-CEB2-4faaFB6BFF329BF39FA1E4",
-        ),
-        (
-            invalid_character("-", 'F', 23),
-            "F9168C5E-CEB2-4faa-B6BFF329BF39FA1E4",
-        ),
-        (
-            invalid_character("-", 'X', 18),
-            "F9168C5E-CEB2-4faaXB6BFF329BF39FA1E4",
-        ),
-        (
-            invalid_character(HEX_DIGITS, '%', 15),
-            "67e5504410b1426%9247bb680e5fe0c8",
-        ),
-        (
-            invalid_character(HEX_DIGITS, 'X', 6),
-            "67e550X410b1426f9247bb680e5fe0cd",
-        ),
-        (
-            invalid_character(HEX_DIGITS, '-', 6),
-            "67e550-4105b1426f9247bb680e5fe0c",
-        ),
-        (
-            invalid_character(HEX_DIGITS, '-', 12),
-            "F9168C5E-CEB-24fa-eB6BFF32-BF39FA1E4",
-        ),
+        (B2, "F9168C5E-CEB2-4faa-B6BF1-02BF39FA1E4"),
+        (B2, "231231212212423424324323477343246663"),
+        (B2, "01020304-1112-2122-3132-41424344"),
+        (B2, "F9168C5E-CEB2-4faa-BGBF-329BF39FA1E4"),
+        (B2, "F9168C5E-CEB2F4faaFB6BFF329BF39FA1E4"),
+        (B2, "F9168C5E-CEB2-4faaFB6BFF329BF39FA1E4"),
+        (B2, "F9168C5E-CEB2-4faa-B6BFF329BF39FA1E4"),
+        (B2, "F9168C5E-CEB2-4faaXB6BFF329BF39FA1E4"),
+        (B2, "67e5504410b1426%9247bb680e5fe0c8"),
+        (B2, "67e550X410b1426f9247bb680e5fe0cd"),
+        (B2, "67e550-4105b1426f9247bb680e5fe0c"),
+        (B2, "F9168C5E-CEB-24fa-eB6BFF32-BF39FA1E4"),
     ];
 
     for &(ref expected, input) in ERR {

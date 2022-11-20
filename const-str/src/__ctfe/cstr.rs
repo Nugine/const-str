77e0f2643f18de8e45e555724fba43d53daa9b1e
Nugine <nugine@foxmail.com>
2022-11-20 14:06:12 +0800

    const impl cstr!

--- const-str/src/__ctfe/cstr.rs
+++ const-str/src/__ctfe/cstr.rs
@@ -40,6 +40,8 @@ impl ToCStr<&str> {
 /// This trailing nul byte will be appended by this macro.
 /// The provided data should not contain any nul bytes in it.
 ///
+/// See also [`cstr!`](crate::cstr)
+///
 /// # Examples
 ///
 /// ```
@@ -48,10 +50,32 @@ impl ToCStr<&str> {
 /// ```
 #[macro_export]
 macro_rules! raw_cstr {
-    ($s: expr) => {{
+    ($s: expr) => {
+        $crate::cstr!($s).as_ptr()
+    };
+}
+
+/// Converts a string slice to [`&CStr`](core::ffi::CStr).
+///
+/// The C-style string is guaranteed to be terminated by a nul byte.
+/// This trailing nul byte will be appended by this macro.
+/// The provided data should not contain any nul bytes in it.
+///
+/// See also [`raw_cstr!`](crate::raw_cstr)
+///
+/// # Examples
+///
+/// ```
+/// use core::ffi::CStr;;
+/// const PRINTF_FMT: &CStr = const_str::cstr!("%d\n");
+/// ```
+#[macro_export]
+macro_rules! cstr {
+    ($s:expr) => {{
         const OUTPUT_LEN: ::core::primitive::usize = $crate::__ctfe::ToCStr($s).output_len();
         const OUTPUT_BUF: [u8; OUTPUT_LEN] = $crate::__ctfe::ToCStr($s).const_eval();
-        const OUTPUT: *const ::core::ffi::c_char = OUTPUT_BUF.as_ptr().cast();
+        const OUTPUT: &::core::ffi::CStr =
+            unsafe { ::core::ffi::CStr::from_bytes_with_nul_unchecked(&OUTPUT_BUF) };
         OUTPUT
     }};
 }

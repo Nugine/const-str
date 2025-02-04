93edb76f4d2dd22b059e031412fd32ebd4dcd219
Nugine <nugine@foxmail.com>
2025-02-04 16:27:30 +0800

    feat: add `split_lines!`

--- const-str/src/__ctfe/split.rs
+++ const-str/src/__ctfe/split.rs
@@ -4,11 +4,16 @@ use crate::utf8::CharEncodeUtf8;
 
 use core::str;
 
-pub struct Split<T, P>(pub T, pub P);
+struct SplitImpl<'input, 'pat> {
+    input: &'input str,
+    pattern: &'pat str,
+    inclusive: bool,
+}
 
-impl<'input> Split<&'input str, &str> {
-    pub const fn output_len(&self) -> usize {
-        let Self(mut input, pat) = *self;
+impl<'input> SplitImpl<'input, '_> {
+    const fn output_len(&self) -> usize {
+        let mut input = self.input;
+        let pat = self.pattern;
 
         if pat.is_empty() {
             crate::utf8::str_count_chars(input) + 2
@@ -23,8 +28,9 @@ impl<'input> Split<&'input str, &str> {
     }
 
     #[allow(unsafe_code)]
-    pub const fn const_eval<const N: usize>(&self) -> [&'input str; N] {
-        let Self(mut input, pat) = *self;
+    const fn const_eval<const N: usize>(&self) -> [&'input str; N] {
+        let mut input = self.input;
+        let pat = self.pattern;
 
         let mut buf: [&str; N] = [""; N];
         let mut pos = 0;
@@ -49,7 +55,11 @@ impl<'input> Split<&'input str, &str> {
             }
         } else {
             while let Some((m, remain)) = crate::str::next_match(input, pat) {
-                let substr = subslice(input.as_bytes(), 0..m);
+                let substr = if self.inclusive {
+                    subslice(input.as_bytes(), 0..m + pat.len())
+                } else {
+                    subslice(input.as_bytes(), 0..m)
+                };
                 buf[pos] = unsafe { str::from_utf8_unchecked(substr) };
                 pos += 1;
                 input = remain;
@@ -62,15 +72,44 @@ impl<'input> Split<&'input str, &str> {
     }
 }
 
+pub struct Split<T, P>(pub T, pub P);
+
+impl<'input, 'pat> Split<&'input str, &'pat str> {
+    const fn to_impl(&self) -> SplitImpl<'input, 'pat> {
+        SplitImpl {
+            input: self.0,
+            pattern: self.1,
+            inclusive: false,
+        }
+    }
+
+    pub const fn output_len(&self) -> usize {
+        self.to_impl().output_len()
+    }
+
+    #[allow(unsafe_code)]
+    pub const fn const_eval<const N: usize>(&self) -> [&'input str; N] {
+        self.to_impl().const_eval()
+    }
+}
+
 impl<'input> Split<&'input str, char> {
+    const fn to_impl<'pat>(&self, ch: &'pat CharEncodeUtf8) -> SplitImpl<'input, 'pat> {
+        SplitImpl {
+            input: self.0,
+            pattern: ch.as_str(),
+            inclusive: false,
+        }
+    }
+
     pub const fn output_len(&self) -> usize {
         let ch = CharEncodeUtf8::new(self.1);
-        Split(self.0, ch.as_str()).output_len()
+        self.to_impl(&ch).output_len()
     }
 
     pub const fn const_eval<const N: usize>(&self) -> [&'input str; N] {
         let ch = CharEncodeUtf8::new(self.1);
-        Split(self.0, ch.as_str()).const_eval()
+        self.to_impl(&ch).const_eval()
     }
 }
 
@@ -100,9 +139,105 @@ impl<'input> Split<&'input str, char> {
 #[macro_export]
 macro_rules! split {
     ($s: expr, $pat: expr) => {{
-        const OUTPUT_LEN: usize = $crate::__ctfe::Split($s, $pat).output_len();
-        const OUTPUT_BUF: [&str; OUTPUT_LEN] = $crate::__ctfe::Split($s, $pat).const_eval();
+        const INPUT: &str = $s;
+        const OUTPUT_LEN: usize = $crate::__ctfe::Split(INPUT, $pat).output_len();
+        const OUTPUT_BUF: [&str; OUTPUT_LEN] = $crate::__ctfe::Split(INPUT, $pat).const_eval();
         OUTPUT_BUF
+    }};
+}
+
+pub struct SplitInclusive<'input, 'pat>(pub &'input str, pub &'pat str);
+
+impl<'input, 'pat> SplitInclusive<'input, 'pat> {
+    const fn to_impl(&self) -> SplitImpl<'input, 'pat> {
+        SplitImpl {
+            input: self.0,
+            pattern: self.1,
+            inclusive: true,
+        }
+    }
+
+    pub const fn output_len(&self) -> usize {
+        self.to_impl().output_len()
+    }
+
+    pub const fn const_eval<const N: usize>(&self) -> [&'input str; N] {
+        self.to_impl().const_eval()
+    }
+}
+
+pub struct LinesMap<'a, const M: usize>(pub [&'a str; M]);
+
+impl<const M: usize> LinesMap<'_, M> {
+    pub const fn output_len(&self) -> usize {
+        if let Some(s) = self.0.last() {
+            if s.is_empty() {
+                return M - 1;
+            }
+        }
+        M
+    }
+
+    pub const fn const_eval<const N: usize>(&self) -> [&str; N] {
+        let mut buf = [""; N];
+        let mut i = 0;
+        while i < N {
+            let s = self.0[i];
+            match crate::str::strip_suffix(s, "\r\n") {
+                Some(s) => buf[i] = s,
+                None => match crate::str::strip_suffix(s, "\n") {
+                    Some(s) => buf[i] = s,
+                    None => buf[i] = s,
+                },
+            }
+            i += 1;
+        }
+        buf
+    }
+}
+
+/// Returns an array of the lines in a string.
+///
+/// Lines are split by LF (`\n`) or CRLF (`\r\n`).
+///
+/// Line terminators are not included in the returned array.
+///
+/// The final line ending is optional.
+/// A string that ends with a final line ending will return the same lines
+/// as an otherwise identical string without a final line ending.
+///
+/// This macro is [const-context only](./index.html#const-context-only).
+///
+/// See also [`str::lines`](https://doc.rust-lang.org/std/primitive.str.html#method.lines)
+///
+/// # Examples
+/// ```rust
+/// const TEXT: &str = "foo\r\nbar\n\nbaz\r";
+/// const LINES_ARRAY: [&str;4] = const_str::split_lines!(TEXT);
+/// const LINES_SLICE: &[&str] = &const_str::split_lines!(TEXT);
+///
+/// assert_eq!(LINES_ARRAY, LINES_SLICE);
+/// assert_eq!(LINES_SLICE, &["foo", "bar", "", "baz\r"]);
+/// ```
+/// ```rust
+/// const TEXT1: &str = "1\r\n2\r\n3\r\n";
+/// const TEXT2: &str = "1\n2\n3\n";
+/// const TEXT3: &str = "1\n2\n3";
+/// const LINES1: &[&str] = &const_str::split_lines!(TEXT1);
+/// const LINES2: &[&str] = &const_str::split_lines!(TEXT2);
+/// const LINES3: &[&str] = &const_str::split_lines!(TEXT3);
+/// assert_eq!(LINES1, LINES2);
+/// assert_eq!(LINES2, LINES3);
+/// ```
+#[macro_export]
+macro_rules! split_lines {
+    ($s: expr) => {{
+        const INPUT: &str = $s;
+        const M: usize = $crate::__ctfe::SplitInclusive(INPUT, "\n").output_len();
+        const BUF: [&str; M] = $crate::__ctfe::SplitInclusive(INPUT, "\n").const_eval();
+        const N: usize = $crate::__ctfe::LinesMap(BUF).output_len();
+        const ANS: [&str; N] = $crate::__ctfe::LinesMap(BUF).const_eval();
+        ANS
     }};
 }
 
@@ -114,12 +249,11 @@ mod tests {
     fn test_split() {
         macro_rules! testcase {
             ($input: expr, $pat: expr) => {{
-                const OUTPUT_LEN: usize = Split($input, $pat).output_len();
-                const OUTPUT: &[&str] = &Split($input, $pat).const_eval::<OUTPUT_LEN>();
+                const OUTPUT: &[&str] = &$crate::split!($input, $pat);
 
                 let ans = $input.split($pat).collect::<Vec<_>>();
+                assert_eq!(OUTPUT.len(), ans.len());
                 assert_eq!(OUTPUT, &*ans, "ans = {:?}", ans);
-                assert_eq!(OUTPUT_LEN, ans.len());
             }};
         }
 
